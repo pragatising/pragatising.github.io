37327b04fe80fe98d9c8d3f4bbffc48d4a48bb99
Pragati Singh <34302400+pragatising@users.noreply.github.com>
2019-09-23 14:33:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//     $(".container1").load("home.html");
+// });
+
+
+    // $(this).on("click", function(){
+    //     var el=this; 
+        
+
+    // });
+
+// $( this ).click(function( event ) {
+   
+//   $(".home").remove();
+//          $(".container1").load(event.target.getAttribute("data-page"));
+// });
+
+
+
+   
