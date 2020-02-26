764098ad3b2d87be08c84e9ed4a63fb3bd301000
Pragati Singh <34302400+pragatising@users.noreply.github.com>
2020-02-26 04:10:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-function jumpScroll() {
-       window.scroll(0,0); 
-       document.getElementById("focus").focus();
-}
 
 var btn = $('#button');
 
@@ -17,6 +13,32 @@ $(window).scroll(function() {
 btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '100');
+});
+
+ document.getElementById("focus").focus();
+
+$(".image").click(function(){
+
+$(".modal").addClass("is-active");
+
+
+});
+
+$(".modal-close").click(function(){
+$(".modal").removeClass("is-active");
+
+});
+
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
 });
 
 
