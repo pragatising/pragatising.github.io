8155b8227d4ab708fabd960dce571f4a230ca225
Pragati Singh <34302400+pragatising@users.noreply.github.com>
2020-05-07 17:41:29 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -35,19 +35,27 @@ $(".category").click(function () {
 
 
 
-//$(document).ready(function() {
-//
-//  // Check for click events on the navbar burger icon
-//  $(".navbar-burger").click(function() {
-//
-//      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-//      $(".navbar-burger").toggleClass("is-active");
-//      $(".navbar-menu").toggleClass("is-active");
-//
-//  });
-//
-//
-//
-//
-//
-//});
+$(".about-modal").click(function () {
+
+    $(".about").fadeIn();
+     $(".mainpage").fadeOut();
+
+
+
+
+
+
+});
+
+
+$("#close").click(function () {
+
+    $(".about").fadeOut();
+     $(".mainpage").fadeIn();
+
+
+
+
+
+
+});
